f0493246e882ace6264e7799de92d979006799f0
Filip Žitný <filipzitny@gmail.com>
2024-01-09 03:57:42 +0100

    wall test

--- tests/st_emmeram/map/wall_test.py
+++ tests/st_emmeram/map/wall_test.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
 """Package containing tests for module wall."""
 import unittest
+import logging
+from PIL import Image
+from st_emmeram.map.wall import Wall
 
+MODULE_LOGGER = logging.getLogger(__name__)
+MODULE_LOGGER.setLevel(logging.INFO)
+
+
+class WallTest(unittest.TestCase):
+    """Test class for class Wall."""
+
+    def get_texture_test(self):
+        """Test the method get_texture."""
+        wall = Wall()
+        texture = wall.get_texture()
+        self.assertIsInstance(texture, Image.Image)
+
+
+if __name__ == '__main__':
+    unittest.main()
