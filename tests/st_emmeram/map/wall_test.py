0d3010d58ec7427181982b6336bbe9f428e87e7f
Filip Žitný <filipzitny@gmail.com>
2024-01-09 04:12:14 +0100

    wall test

--- tests/st_emmeram/map/wall_test.py
+++ tests/st_emmeram/map/wall_test.py
@@ -1,22 +1,47 @@
 # -*- coding: utf-8 -*-
 """Package containing tests for module wall."""
+import os
 import unittest
 import logging
 from PIL import Image
 from st_emmeram.map.wall import Wall
+from st_emmeram.settings import walls_folder
 
 MODULE_LOGGER = logging.getLogger(__name__)
 MODULE_LOGGER.setLevel(logging.INFO)
 
 
-class WallTest(unittest.TestCase):
+class TestWall(unittest.TestCase):
     """Test class for class Wall."""
 
-    def get_texture_test(self):
+    def setUp(self):
+        """Set up the test."""
+        MODULE_LOGGER.info('Start test.')
+        self.wall_folder = walls_folder
+
+    def test_get_texture(self):
         """Test the method get_texture."""
         wall = Wall()
+
+        # Call the method
         texture = wall.get_texture()
+
+        # Assertions
         self.assertIsInstance(texture, Image.Image)
+
+        self.assertIsInstance(texture, Image.Image)
+
+        # Check if the returned image matches one of the test images
+        matches_one_of_the_images = False
+        for file_name in os.listdir(self.wall_folder):
+            file_path = os.path.join(self.wall_folder, file_name)
+            if os.path.isfile(file_path):
+                with Image.open(file_path) as test_image:
+                    if list(texture.getdata()) == list(test_image.getdata()):
+                        matches_one_of_the_images = True
+                        break
+
+        self.assertTrue(matches_one_of_the_images)
 
 
 if __name__ == '__main__':
