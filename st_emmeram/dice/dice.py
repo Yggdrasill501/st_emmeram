87026b735a02074a12a05c8284ef74a3e383d35b
Filip Žitný <filipzitny@gmail.com>
2024-01-10 00:48:31 +0100

    decies

--- st_emmeram/dice/dice.py
+++ st_emmeram/dice/dice.py
@@ -12,27 +12,31 @@ class Dice:
         self.images = [pygame.transform.scale(pygame.image.load(f"/Users/yggdrasill501/Projects/code/python/st_emmeram/st_emmeram/assets/dice/{i}.png"), size=size) for i in range(1, 7)]
         self.current_value = 1
         self.rect = self.images[0].get_rect(topleft=position)
+        self.rolled = False
 
-    def roll(self) -> None:
-        """Roll the dice."""
+    def roll(self):
         self.current_value = random.randint(1, 6)
+        self.rolled = True
 
     def draw(self, screen) -> None:
         """Draw the dice."""
         screen.blit(self.images[self.current_value - 1], self.rect)
 
     def get_value(self) -> int:
-        """Get the value of the dice."""
+        """Return the current value of the dice."""
         return self.current_value
 
-    def on_click(self, event_position) -> bool:
-        """Check if the dice was clicked."""
-        if self.rect.collidepoint(event_position):
-            self.roll()
-            return True
-        return False
+    def has_been_rolled(self) -> bool:
+        """Return True if the dice has been rolled, False otherwise."""
+        return self.rolled
+
+    def reset(self) -> None
+        """Reset the dice."""
+        self.rolled = False
 
     @staticmethod
-    def get_sum(dice1, dice2) -> int:
-        """Get the sum of two dice."""
-        return dice1.get_value() + dice2.get_value()
+    def calculate_sum(dice1, dice2) :
+        """Calculate the sum of two dice."""
+        if dice1.has_been_rolled() and dice2.has_been_rolled():
+            return dice1.get_value() + dice2.get_value()
+        return None
