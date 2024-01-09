b23669b3116ac9a3711aa5e515ca949c9bce4a80
Filip Žitný <filipzitny@gmail.com>
2024-01-09 08:02:12 +0100

    Player player movent etc

--- st_emmeram/game_loop.py
+++ st_emmeram/game_loop.py
@@ -4,6 +4,7 @@
 import pygame
 import sys
 from st_emmeram.dice.dice import Dice
+from st_emmeram.player.player import Player
 
 
 class GameLoop:
@@ -15,6 +16,9 @@ class GameLoop:
 
         self.dice1 = Dice((50, 50))
         self.dice2 = Dice((200, 50))
+
+        self.player = Player((80, 80), (self.width, self.height))
+
         self.font = pygame.font.Font(None, 36)
         self.sum_of_dice = 0
 
@@ -29,9 +33,21 @@ class GameLoop:
                     if self.dice1.on_click(event.pos) or self.dice2.on_click(event.pos):
                         self.sum_of_dice = Dice.get_sum(self.dice1, self.dice2)
 
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_UP] or keys[pygame.K_w]:
+                    self.player.move("up")
+                if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                    self.player.move("down")
+                if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                    self.player.move("left")
+                if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                    self.player.move("right")
+
             self.screen.fill((0, 128, 255))
             self.dice1.draw(self.screen)
             self.dice2.draw(self.screen)
+
+            self.player.draw(self.screen)
 
             sum_text = self.font.render(f'Sum: {self.sum_of_dice}', True, (0, 0, 0))
             self.screen.blit(sum_text, (50, 150))
--- st_emmeram/player/player.py
+++ st_emmeram/player/player.py
@@ -6,23 +6,47 @@ import pygame
 class Player:
     """Class for the player."""
 
-    def __init__(self, position, size=(50, 50)) -> None:
+    def __init__(self, position, screen_size) -> None:
         """Initialize the player."""
-        self.rect = pygame.Rect(position[0], position[1], size[0], size[1])
-        self.color = (0, 255, 0)
-        self.speed = 5
+        self.size = (50, 50)
+        self.screen_width, self.screen_height = screen_size
+
+        self.image_centered = pygame.transform.scale(pygame.image.load("/Users/yggdrasill501/Projects/code/python/st_emmeram/st_emmeram/assets/player/vales_centered.png"), size=self.size)
+        self.image_right = pygame.transform.scale(pygame.image.load("/Users/yggdrasill501/Projects/code/python/st_emmeram/st_emmeram/assets/player/vales_right.png"), size=self.size)
+        self.image_left = pygame.transform.scale(pygame.image.load("/Users/yggdrasill501/Projects/code/python/st_emmeram/st_emmeram/assets/player/vales_left.png"), size=self.size)
+
+        self.image = self.image_centered
+
+        self.rect = self.image.get_rect(topleft=position)
+        self.speed = 10
 
     def move(self, direction) -> None:
         """Move the player."""
         if direction == "up":
             self.rect.y -= self.speed
+            self.image = self.image_centered
         elif direction == "down":
             self.rect.y += self.speed
+            self.image = self.image_centered
         elif direction == "left":
             self.rect.x -= self.speed
+            self.image = self.image_left
         elif direction == "right":
             self.rect.x += self.speed
+            self.image = self.image_right
+
+        self.check_boundaries()
+
+    def check_boundaries(self) -> None:
+        if self.rect.left < 0:
+            self.rect.right = self.screen_width
+        elif self.rect.right > self.screen_width:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.bottom = self.screen_height
+        elif self.rect.bottom > self.screen_height:
+            self.rect.top = 0
 
     def draw(self, screen):
         """Draw the player."""
-        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.image, self.rect)
