29c5763daecc402c5bd3bd2cc72e3a9894e42d14
Filip Žitný <filipzitny@gmail.com>
2024-02-04 23:28:31 +0100

    Monster stats

---
+++ src/st_emmeram/sprites/stats.py
@@ -0,0 +1,80 @@
+monsters: dict = {
+    "chest": {
+        "skin": "assets/monsters/chest.png",
+        "Amount": 10,
+        "damage": 0,
+        "drop": {
+
+        }
+    },
+    "rat": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 8,
+        "damage": 5,
+        "drop": {
+            "type": "dagger",
+            "skin": "",
+            "damage": 5
+        }
+    },
+    "spider": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 4,
+        "damage": 6,
+        "drop": {
+            "type": "health",
+            "skin": "",
+            "damage": 0
+        }
+    },
+    "mummy": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 8,
+        "damage": 7,
+        "drop": {
+            "type": "health",
+            "skin": "",
+            "damage": 7
+        }
+    },
+    "skeleton": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 12,
+        "damage": 8,
+        "drop": {
+            "type": "health",
+            "skin": "",
+            "damage": 8
+        }
+    },
+    "warrior": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 4,
+        "damage": 9,
+        "drop": {
+            "type": "health",
+            "skin": "",
+            "damage": 9
+        }
+    },
+    "king": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 2,
+        "damage": 10,
+        "drop": {
+            "type": "health",
+            "skin": "",
+            "damage": 10
+        }
+    },
+    "creature": {
+        "skin": "assets/monsters/rat.png",
+        "Amount": 2,
+        "damage": 12,
+        "drop": {
+            "type": "open-chest",
+            "skin": "",
+            "damage": 0
+        }
+    }
+}
